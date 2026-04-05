7b0918f34795f0cfc05b50534eaaa574fca9a6a7
InfernoStats <77599829+InfernoStats@users.noreply.github.com>
2026-04-05 18:37:27 -0400

    Add Magic Imbue Support (#44)

---
+++ src/main/java/com/portaguy/infoboxes/MagicImbueInfobox.java
@@ -0,0 +1,28 @@
+package com.portaguy.infoboxes;
+
+import com.portaguy.SpellReminderConfig;
+import com.portaguy.SpellReminderInfobox;
+import com.portaguy.SpellReminderPlugin;
+import com.portaguy.trackers.MagicImbueTracker;
+import net.runelite.api.SpriteID;
+
+import javax.inject.Inject;
+
+public class MagicImbueInfobox extends SpellReminderInfobox {
+  @Inject
+  public MagicImbueInfobox(SpellReminderPlugin plugin,
+                           SpellReminderConfig config,
+                           MagicImbueTracker tracker) {
+    super(plugin, config, tracker);
+  }
+
+  @Override
+  protected int getSpriteId() {
+    return SpriteID.SPELL_MAGIC_IMBUE;
+  }
+
+  @Override
+  protected int getTimeoutSeconds() {
+    return config.magicImbueTimeoutSeconds();
+  }
+}
